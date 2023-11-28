178aafe8abcb2f2b758aa0f62f788aad6eaeeeff
MinhaHwang <minha0616@naver.com>
2023-11-28 21:44:30 +0900

    movie start

--- src/App.js
+++ src/App.js
@@ -1,59 +1,26 @@
-import { useState } from "react";
 /*
-form은 submit 이벤트를 갖고 있다.
-그러므로 evernt.preventDefault() 함수를 이용하여 기본 동작을 막자.
-여러 개의 toDo를 받을 수 있는 배열 만들기
-const [toDos, setToDos] = useState([]); -> 기본값은 비어있는 배열
-state는 직접적으로 수정 불가능 (예 : toDo = “” )
-함수를 가져와서 수정하게 만들어야함 (예 : setToDo = (“”) )
-그래서 toDos 배열을 수정하고 싶다면 수정하는 함수를 써야함
+react-router-dom 5버전 -> 버전6 바뀐 부분
 
-setToDos(currentArray => [toDo, ...currentArray]);
--> ...을 써서 currentArray 배열에 toDo를 추가 시켜줌
-어플리케이션이 시작될 때는 비어있는 배열을 가짐
-첫 번째 to-do를 입력할 때 비어있는 currentArray 받아옴
-이건 새로운 toDos가 input을 통해 작성한 toDo와
-아무것도 들어있지 않은 빈 배열의 element가 더해지게 된다는 것
-첫 번째 toDo 가 Hello라면 엔터를 눌러 실행됨
-그리고 byebye라고 적으면
-currentArray에는 Hello 이미 있고 toDo는 byebye가 되는 것
-그리고 currentArray는 Hello와 byebye를 가지고 있는 배열이 됨
+1. Switch컴포넌트가 Routes컴포넌트로 대체됨
+Switch -> Routes
+
+2. Route컴포넌트 사이에 자식 컴포넌트를 넣지 않고, element prop에 자식 컴포넌트를 할당하도록 바뀜
+Route path="/" element={< Home / >}
 */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") {
-            return;
-        }
-        setToDos((currentArray) => [toDo, ...currentArray]);
-        setToDo("");
-    };
-    const deleteBtn = (index) => {
-        setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
-    };
-    console.log(toDos);
-    console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
     return (
-        <div>
-            <h1>My To Dos ({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
-                <button>Add To Do</button>
-            </form>
-            <hr />
-            <ul>
-                {toDos.map((item, index) => (
-                    <li key={index}>
-                        {item}
-                        <button onClick={() => deleteBtn(index)}>❌</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/movie" element={<Detail />}></Route>
+            </Routes>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+            </Routes>
+        </Router>
     );
 }
 
---
+++ src/routes/Home.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+/*
+Movie 컴포넌트는 medium_cover_image, title, summary, genres
+-> 이 props를 모두 부모 컴포넌트로부터 받아옴
+
+coverImg={movie.medium_cover_image}
+자바스크립트에서는 medium_cover_image가 아닌mediumCoverImage로 쓰지만
+내가 만든 컴포넌트라 아무렇게 써도 됨.
+그러나 movie.medium_cover_image 에서는 API에서 가져오므로 API 정보와 똑같이 써야함
+이미지 element들을 alt속성을 가짐 -> alt={title}
+
+
+home 라우트(페이지)는 모든 영화를 보여주고
+Movie 라우트는 영화 하나만 보여줌
+이렇게 라우트 별로 생각해야함
+home 라우트는 기본적으로 App 컴포넌트 전체를 가지고 있게 만듦
+
+App에 있는 것을 모두 Home라우트로 옮겼으니
+App.js는 라우터를 렌더한다.
+*/
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json();
+        //const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
+    useEffect(() => {
+        /*fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+            .then((response) => response.json())
+            .then((json) => setMovies(json.data.movies);
+            setLoading(false));*/
+        getMovies();
+    }, []);
+
+    return (
+        <div>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div>
+                    {movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            coverImg={movie.medium_cover_image}
+                            title={movie.title}
+                            summary={movie.summary}
+                            genres={movie.genres}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+export default Home;
